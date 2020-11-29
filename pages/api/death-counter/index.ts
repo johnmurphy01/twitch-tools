e201906aa922823b353166e9ab77d57a0a419cf5
John Murphy <jmichealmurphy@gmail.com>
2020-11-28 20:33:39 -0600

    move pusher ids to environment

--- pages/api/death-counter/index.ts
+++ pages/api/death-counter/index.ts
@@ -7,9 +7,9 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
     switch (req.method) {
       case RequestMethodType.Get:
         const pusher = new Pusher({
-          appId: "1114424",
-          key: "9dfae5ce4b0f269dded0",
-          secret: "c53299f2c1742670d878",
+          appId: process.env.PUSHER_APP_ID,
+          key: process.env.PUSHER_KEY,
+          secret: process.env.PUSHER_SECRET,
           cluster: "mt1",
           useTLS: true,
         });
