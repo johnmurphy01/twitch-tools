75eb3d1b5c62e7bbef246a57afd69b9d89241895
John Murphy <jmichealmurphy@gmail.com>
2020-11-28 20:29:36 -0600

    adjust the button a bit

--- pages/death-counter-management/index.tsx
+++ pages/death-counter-management/index.tsx
@@ -5,7 +5,7 @@ const DeathCounterManagement: React.FC = () => {
     <div style={{ backgroundColor: "#ddd", display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
       <button
         className="btn btn-lg btn-success"
-        style={{ borderRadius: "5em", fontSize: "4.25rem" }}
+        style={{ borderRadius: "5em", fontSize: "4.25rem", lineHeight: "1.5", paddingTop: 0 }}
         onClick={async () => {
           await fetch(new URL("api/death-counter", process.env.BASE_URL).toString());
         }}
